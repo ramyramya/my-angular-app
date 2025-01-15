27332b31f6268415e0facd345555848f0661d2c7
SENAPATI SAI RAMYA <ramyasenapati2004@gmail.com>
2025-01-15 20:07:26 +0530

    commit

--- frontend/src/app/features/dashboard/services/dashboard.service.ts
+++ frontend/src/app/features/dashboard/services/dashboard.service.ts
@@ -43,7 +43,7 @@ export class DashboardService {
   downloadProductAsPDF(product: Product){
     const doc = new jsPDF();
         // Add product image (Make sure `product.image_url` is the correct URL or base64 string)
-        const imageUrl = "https://akv-interns.s3.ap-south-1.amazonaws.com/profile-photos/kurkure.jpg";
+        const imageUrl = product.product_image;
         console.log(imageUrl);  // Example: product.product_image_url (URL of the image)
   
         // Add the image to the PDF (adjust the parameters as needed)
